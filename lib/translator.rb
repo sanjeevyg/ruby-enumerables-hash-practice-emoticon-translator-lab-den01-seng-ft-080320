f1a8fc635620826e83acd4c0b462cbd3656e8208
Sanjeev Yogi <yogi.sjv@gmail.com>
2020-05-25 07:33:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,7 +27,6 @@ def get_japanese_emoticon(file_path, emoticon)
   hash_1 = load_library("./lib/emoticons.yml")
   hash_1.each {|key, value|
     value.each {|key_1, value_1|
-    binding.pry
       if emoticon == key[:english]
          emoticon = key[:japanese]
       binding.pry
